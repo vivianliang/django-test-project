445031a7c849c86a1ea7512deae41157610f1a84
Vivian Liang <vliang88@gmail.com>
2015-01-21 14:40:03 -0800

    Customize admin page, field order

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from polls.models import Question
 
+
 # Register your models here.
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
